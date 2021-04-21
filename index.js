4783e146b29ba0b9139acce03da560eb52945f7e
DcodeDesign  <gravythomas@gmail.com>
2021-04-21 15:32:21 +0200

    [ADD]: branch passportjs

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const passport = require("passport");
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
     res.send('Roster')
 })
+
+app.post('/login', passport.authenticate('local', { successRedirect: '/',
+    failureRedirect: '/login' }));
 
 app.listen(port, () => {
     console.log(`port: ${port}`)
