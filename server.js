3b64e652769ea67d33f07ad694c0e5f53857c69b
DcodeDesign  <gravythomas@gmail.com>
2021-04-22 12:18:57 +0200

    [ADD]: configuration des variables d'environnement [UPDATE]: server.js -> implémentation de dotenv

--- server.js
+++ server.js
@@ -5,12 +5,18 @@ const passport = require("passport")
 // const cookieParser = require('cookie-parser') # à remplacer par le storage
 const session = require('express-session')
 const bodyParser = require('body-parser')
-const port = 3000
+const path = require('path');
+const dotenv = require("dotenv")
+dotenv.config( { path: dotenv.config().parsed["IS_LOCAL"] === 'true' ? path.join(__dirname, `./environments/.env.dev`) : path.join(__dirname, `./environments/.env.prod`)})
+console.log(process.env.ENV)
 
+// require('./strategies/passport-local')(passport);
 // app.use(cookieParser()); # à remplacer par le storage
+
 app.use(session({
-    secret: 'secret'
+    secret: process.env.SECRET
 }))
+
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(bodyParser.json())
@@ -18,8 +24,8 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
-require('./routes/auth.js')(app, passport)
+// require('./routes/auth.js')(app, passport)
 
-app.listen(port, () => {
-    console.log(`port: ${port}`)
+app.listen(process.env.PORT, () => {
+    console.log(`Environnement: ${process.env.ENV} | version: ${process.env.VERSION} | port: ${process.env.PORT}`)
 })
